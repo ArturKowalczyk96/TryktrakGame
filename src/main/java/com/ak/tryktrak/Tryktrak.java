55e5ecfb042b049e10bb8c482d274d2e89be6f38
Artur Kowalczyk <artur.kowalczyk96@gmail.com>
2020-10-25 20:51:19 +0100

    Tryktrak - commit 3

--- src/main/java/com/ak/tryktrak/Tryktrak.java
+++ src/main/java/com/ak/tryktrak/Tryktrak.java
@@ -1,29 +1,58 @@
 package com.ak.tryktrak;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Tryktrak extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    private Image imageback = new Image("file:src/main/resources/Table.png");
+    private Image pionWhite = new Image("file:src/main/resources/PionBialy.png");
+    private FlowPane pionsWhite = new FlowPane(Orientation.HORIZONTAL);
+    private Image pionRed = new Image("file:src/main/resources/PionCzerwony.png");
+    private FlowPane pionsRed = new FlowPane(Orientation.HORIZONTAL);
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root, 1000, 1000, Color.BLACK);
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
 
-        Rectangle r = new Rectangle(25,25,950,950);
-        r.setFill(Color.BLUE);
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.TOP_LEFT);
+        grid.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+        grid.setHgap(5.5);
+        grid.setVgap(5.5);
+        grid.setBackground(background);
 
-        root.getChildren().add(r);
+        ImageView imgR = new ImageView(pionRed);
+        pionsRed.getChildren().add(imgR);
+
+        grid.add(pionsRed, 54, 3, 300, 300);
+
+        ImageView imgW = new ImageView(pionWhite);
+        pionsWhite.getChildren().add(imgW);
+
+        grid.add(pionsWhite, 2, 3, 300, 300);
+
+
+
+        Scene scene = new Scene(grid, 1300, 1060, Color.BLACK);
+
         primaryStage.setTitle("Tryktrak");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
